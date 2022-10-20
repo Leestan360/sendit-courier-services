6f7131fbdea1f3d5ab6ce5165c77737646b8beb9
Leestan360 <stanleymayore2@gmail.com>
2022-10-20 08:47:42 +0300

    Create different user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+
+  skip_before_action :authorize, only: [:create, :show]
+
+  def index
+    users = User.all
+    render json: users, status: :ok
+  end
+
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
+  end
+
+  def show
+    render json: @current_user
+  end
+
+  def patch
+    user = User.find(params[:id])
+    user.update!(user_params)
+    render json: user, status: :accepted
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+    head :no_content
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :phone_number, :password, :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
 end
