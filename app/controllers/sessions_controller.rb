965a21a40416fcf4cf1312a8dc7f85949cf0a6a7
Leestan360 <stanleymayore2@gmail.com>
2022-10-20 08:56:35 +0300

    Add serializer to user model

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  skip_before_action :authorize, only: :create
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { errors: ["Invalid email or password"] }, status: :unauthorized
+    end
+  end
+
+  def destroy 
+    if session[:user_id]
+        session.delete :user_id
+        head :no_content
+    else
+      render json: {errors: ["You must be logged in to access this content"] }, status: :unauthorized
+    end
+  end
+
+end
