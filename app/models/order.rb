12d5dc3c8bfe8cb7ceeba9722cf3cb642e88e1c9
koechronix <koechronix@yahoo.com>
2022-10-25 07:34:02 +0300

    Added validations to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,9 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :parcel
 
+  validates :delivery_status, presence: true 
+  validates :delivery_type, presence: true
+  validates :delivery_location, presence: true 
+  validates :pickup_location, presence: true 
+  validates :payment_method, presence: true
 end
