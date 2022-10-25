d1b00998af6fca4388b5ddac7afc0988082cc25a
koechronix <koechronix@yahoo.com>
2022-10-25 14:57:19 +0300

    Updated parcel table

--- db/migrate/20221019162515_create_parcels.rb
+++ db/migrate/20221019162515_create_parcels.rb
@@ -2,11 +2,13 @@ class CreateParcels < ActiveRecord::Migration[7.0]
   def change
     create_table :parcels do |t|
       t.integer :weight
-      t.integer :length
-      t.integer :width
       t.string :category_type
       t.boolean :perishabile
       t.boolean :fragility
+      t.datetime :expected_time
+      t.datetime :delivered_time
+      t.string :delivery_location
+      t.string :name
 
       t.timestamps
     end
