ee683c429f8170abb9bd82d73b4c968ef5e138e9
mmulahvictor <mmulahvictor@gmail.com>
2022-10-26 12:12:26 +0300

    Add admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :parcels, only: [:index, :create, :show, :destroy, :update]
   
 end
