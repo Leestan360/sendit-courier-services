99dee50c559fd89c7c8b7eb8672dc5dc0ba22fe3
koechronix <koechronix@yahoo.com>
2022-10-26 14:25:52 +0300

    Finalized parcels

--- app/serializers/parcel_serializer.rb
+++ app/serializers/parcel_serializer.rb
@@ -1,3 +1,3 @@
 class ParcelSerializer < ActiveModel::Serializer
-  attributes :id, :weight, :category_type, :perishabile, :fragility
+  attributes :id, :weight, :pickup_location, :perishable, :fragility, :expected_time, :delivery_location, :category_type
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,57 @@
-# puts "Seeding Data"
+puts "Seeding Data"
 
-# Parcel.create([
-#     {
-#         weight: 3,
-#         category_type: "electronic",
-#         perishabile: true,
-#         fragility: true
-#     },
-#     {
-#         weight: 6,
-#         length: 23,
-#         width: 14,
-#         category_type: "furniture",
-#         perishabile: false,
-#         fragility: false
-#     },
-#     {
-#         weight: 145,
-#         length: 34,
-#         width: 20,
-#         category_type: "automotive",
-#         perishabile: false,
-#         fragility: false
-#     }
-# ])
+Parcel.create([
+    {
+        user_id: 1,
+        weight: 5,
+        pickup_location: "Nairobi",
+        perishable: true,
+        fragility: true,
+        expected_time: "",
+        delivery_location: "Weatlands",
+        category_type: "electronic"
+    },
+    {
+        user_id: 2,
+        weight: 10,
+        pickup_location: "Nairobi",
+        perishable: true,
+        fragility: false,
+        expected_time: "",
+        delivery_location: "Nakuru",
+        category_type: "clothing"
+    }
+    # {
+    #     user_id: 3,
+    #     weight: 40,
+    #     pickup_location: "Nairobi",
+    #     perishable: false,
+    #     fragility: true,
+    #     expected_time: "",
+    #     delivery_location: "Ruiru",
+    #     category_type: "electronic"
+    # }
+])
 
-# puts "Finished Seeding Data"
+User.create([
+  {
+    first_name: "Amos",
+    last_name: "Koech",
+    email: "amoskoech@gmail.com",
+    phone_number: "254712345678",
+    password: "12345678",
+    password_confirmation: "12345678"
+
+  }, 
+  {
+    first_name: "Victor",
+    last_name: "Mmullah",
+    email: "victormmullah@gmail.com",
+    phone_number: "254712345998",
+    password: "12345678",
+    password_confirmation: "12345678"
+
+  }
+])
+
+puts "Finished Seeding Data"
