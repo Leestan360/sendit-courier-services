f4336d0f3762fcb7782960a77e4a196d5ad140b5
Leestan360 <stanleymayore2@gmail.com>
2022-10-27 16:53:54 +0300

    Add login and signup buttons

--- client/src/components/HeroSection/HeroSection.jsx
+++ client/src/components/HeroSection/HeroSection.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { IoIosSend } from 'react-icons/io';
 
 const HeroSection = () => {
   const navigate = useNavigate();
@@ -16,19 +17,19 @@ const HeroSection = () => {
       className=" w-[100%] items-center justify-start shadow-md h-full text-white"
       style={{ background: "#2B4263" }}
     >
-      <div className="flex justify-between px-8 py-2 border-b border-blue-900">
+      <div className="flex justify-between px-8 py-2 border-b border-blue-800">
         <h1 className="text-[34px]">SendIT</h1>
         <div className="my-auto text-xl font-bold">
           <button
-            className="border-2 mr-4 px-4"
-            style={{ borderRadius: "18px" }}
+            className="active:border-2 active:bg-white active:text-blue-800 mr-2 border-2 border-white px-4"
+            style={{ borderRadius: "22px" }}
             onClick={handleNavigateLogin}
           >
             Login
           </button>
           <button
-            className="border-2 px-4"
-            style={{ borderRadius: "18px" }}
+            className="active:border-2 active:bg-white active:text-blue-800 border-2 border-white px-4"
+            style={{ borderRadius: "22px" }}
             onClick={handleNavigateSignup}
           >
             Signup
@@ -44,8 +45,8 @@ const HeroSection = () => {
             SendIT is a courier service that helps users deliver<br></br>parcels
             to different destinations.
           </p>
-          <button className="py-2 rounded-lg text-blue-800 bg-white font-inter font-bold max-w-[160px] w-[100%] text-xl">
-            Get Started
+          <button className="py-2 px-3 rounded-lg text-blue-800 bg-white font-inter font-extrabold max-w-[200px] w-[100%] text-2xl flex">
+            Get Started <IoIosSend className="ml-2"/>
           </button>
         </div>
         <div className="">
