cc7e572314981bcae73ec66761f75f6ecaec74f1
Leestan360 <stanleymayore2@gmail.com>
2022-10-29 12:27:49 +0300

    Add serializer to user and parcel models

--- app/serializers/parcel_serializer.rb
+++ app/serializers/parcel_serializer.rb
@@ -1,5 +1,5 @@
-# class ParcelSerializer < ActiveModel::Serializer
-#   attributes :id, :weight, :pickup_location, :perishable, :fragility, :expected_time, :delivery_location, :category_type
+class ParcelSerializer < ActiveModel::Serializer
+  attributes :id, :weight, :pickup_location, :perishable, :fragility, :expected_time, :delivery_location, :category_type
 
-#   belongs_to :user
-# end
+  belongs_to :user
+end
