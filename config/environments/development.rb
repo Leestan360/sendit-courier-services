c9e08eecd9b319e5a5e6c770f260ef0fa2226cd6
mmulahvictor <mmulahvictor@gmail.com>
2022-10-31 17:03:39 +0300

    add email notification

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,19 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.delivery_method = :smtp
+  host = 'localhost:4000' #replace with your own url
+  config.action_mailer.default_url_options = { :host => host }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => <mmulahvictor@gmail.com>,
+    :password             => <.66Kenyan>,
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
 end
