cc932fac61de0f4f46360611d07ada25908653c8
Leestan360 <stanleymayore2@gmail.com>
2022-11-01 14:35:29 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_01_090350) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_01_100249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "access_tokens", force: :cascade do |t|
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -58,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_090350) do
     t.string "expected_time"
     t.string "delivery_location"
     t.string "category_type"
+    t.string "status"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
