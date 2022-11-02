34a238af6ce5712b2b8eba4fd10083de9fd72add
mmulahvictor <mmulahvictor@gmail.com>
2022-11-02 03:32:39 +0300

    modify the edit button

--- client/src/components/editItem/Edit.js
+++ client/src/components/editItem/Edit.js
@@ -1,34 +1,66 @@
-import React from 'react'
-import Form from './Form';
+import React from 'react';
 import { useState } from 'react';
 
-function Edit ({id, parcel}) {
-    const [ isedit, setEditing ] = useState( false );
+function Edit ( { id, parcel } ) {
+    const [ location, setLocation ] = useState( parcel );
+    const [ isLoading, setIsLoading ] = useState( false );
+    const url = `/parcels/${ id }`;
+
+    const handleSubmit = ( e ) => {
+        e.preventDefault();
+        updateProduct();
+    };
+
+    const updateProduct = () => {
+        fetch( url, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify( {
+                location: location.delivery_location,
+            } ),
+        } )
+            .then( ( res ) => res.json() )
+            .then( ( location ) => {
+                setIsLoading(false)
+                setLocation( location );
+            } )
+            .catch( ( err ) => console.log( 'error: ', err ) );
+
+    };
 
     function handleChange ( e ) {
-        e.preventDefault();
-        if ( e )
-        {
-            setEditing( () => !isedit );
-        }
-        console.log( e.target.id );
-        fetch( `/parcels/${ e.target.id }`, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(),
-        } )
-            .then( ( r ) => r.json() );
-
+        console.log( e.target.value );
+        setLocation( { ...location, [ e.target.name ]: e.target.value } );
     }
 
-  return (
-      <div id='hide-card'>
+    return (
+        <div id='hide-card'>
 
-          { isedit ? <Form id={ id } parcel={ parcel } /> : <button className='edit-btn' onClick={ handleChange } id={ id }>Edit</button> }
-      </div>
-  )
+            <div>
+                <div>
+
+                    <form onSubmit={ handleSubmit }>
+                        <label htmlFor="delivery_location">Delivery Location</label><br />
+                        <input
+                            type="text"
+                            placeholder='delivery location'
+                            name='delivery_location'
+                            defaultValue={ parcel.delivery_location }
+                            id="delivery_location"
+                            value={ location }
+                            onChange={ handleChange } />
+                        <br />
+
+                        <button type="submit" className='btn'>
+                            { isLoading ? "Loading..." : "Submit Item" }
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Edit
+export default Edit;
