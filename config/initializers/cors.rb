01ef373d26d8b0cb1746c2a112f81d178551d6ab
mmulahvictor <mmulahvictor@gmail.com>
2022-11-07 21:10:07 +0300

    changed cors link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://sendit-courier-services.web.app"
+    origins "https://sendit-courier-services.vercel.app"
 
     resource "*",
       headers: :any,
